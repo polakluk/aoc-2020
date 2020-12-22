9ce439225f2f9b30a152648db9fb4baaa73acb98
Lukas Polak <polak.lukas90@gmail.com>
2020-12-22 02:21:55 +0100

    day 13 part 2

--- day-13/main.go
+++ day-13/main.go
@@ -8,6 +8,54 @@ import (
 	"strings"
 )
 
+func findCommonNaive(busIDs []int) int {
+	t := busIDs[0]
+	for {
+		stopLooking := false
+		for idx, val := range busIDs[1:] {
+			if val == -1 {
+				continue
+			}
+			if (t+idx+1)%val > 0 {
+				stopLooking = true
+				break
+			}
+		}
+
+		if !stopLooking {
+			break
+		}
+		t += busIDs[0]
+	}
+
+	return t
+}
+
+func findCommonNormal(busIDs []int) int {
+	step := busIDs[0]
+	actT := step
+	actIdx := 1
+
+	for ; actIdx < len(busIDs); actT += step {
+		// skip "x"
+		for busIDs[actIdx] == -1 {
+			actIdx++
+		}
+		// check whether the current number is divisible by the current busId
+		if (actT+actIdx)%busIDs[actIdx] == 0 {
+			// if it is, then start skipping "actStep" + "actBusId" numbers and search for another match
+			actIdx++
+			if actIdx == len(busIDs) {
+				// this was the last bus so return the value
+				return actT
+			}
+			step *= busIDs[actIdx-1]
+		}
+	}
+	// error state (should have been panic)
+	return -1
+}
+
 func main() {
 	file, _ := os.Open("./input.txt")
 	scanner := bufio.NewScanner(file)
@@ -21,12 +69,15 @@ func main() {
 
 	waitTime := -1
 	catchBusID := -1
+	buses := []int{}
 	for _, rawBusID := range ids {
 		// skip malfunctioned buses
 		if rawBusID == "x" {
+			buses = append(buses, -1)
 			continue
 		}
 		busID, _ := strconv.Atoi(rawBusID)
+		buses = append(buses, busID)
 		// calculate wait time for this bus
 		actWaitTime := busID - (arrivalTime % busID)
 		if catchBusID == -1 || waitTime > actWaitTime {
@@ -37,4 +88,6 @@ func main() {
 	}
 
 	fmt.Printf("Part 1 answer - %d\n", catchBusID*waitTime)
+	fmt.Printf("Part 2 (normal) - %d\n", findCommonNormal(buses))
+	fmt.Printf("Part 2 (naive) -  %d\n", findCommonNaive(buses))
 }
